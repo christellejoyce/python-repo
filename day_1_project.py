2a0f905b7287a40b26e979fcd85763c8de768f2c
christellejoyce <97485242+christellejoyce@users.noreply.github.com>
2022-01-13 14:07:51 +1100

    Update day_1_project.py

--- day_1_project.py
+++ day_1_project.py
@@ -1,10 +1,34 @@
-#1. Create a greeting for your program.
+# Exercise 1 - Printing
+
+print ("Day 1 - Python Print Function")
+print ("The function is declared like this:")
+print ("print('what to print')")
+
+# Exercise 2 - Debugging Practice
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Exercise 3 - Input Function
+
+print(len(input("What is your name?")))
+
+# Exercise 4 - Variables
+
+a = input("a: ")
+b = input("b: ")
+c = a
+d = b
+a = d
+b = c
+print("a: " + a)
+print("b: " + b)
+
+# Day 1 Project 
+
 print("Hello! Welcome to Band Name Generator.")
-#2. Ask the user for the city that they grew up in.
 city = input("What city did you grow up in?\n")
-#3. Ask the user for the name of a pet.
 pet = input("What is the name of your pet?\n")
-#4. Combine the name of their city and pet and show them their band name.
 print(city + " " + pet + " " + "sounds like a sensational band name!")
-#5. Make sure the input cursor shows on a new line, see the example at:
-#   https://replit.com/@appbrewery/band-name-generator-end
