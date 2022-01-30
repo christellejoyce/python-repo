803ecc9f27ab95132d622959927761784c39c533
Christelle Salimbangon <97485242+christellejoyce@users.noreply.github.com>
2022-01-30 13:17:38 +1100

    Add files via upload

---
+++ day_13_answers.py
@@ -0,0 +1,12 @@
+for number in range(1, 101):
+  #or should be and
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  #if should be elif
+  elif number % 3 == 0:
+    print("Fizz")
+  #if should be elif
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
